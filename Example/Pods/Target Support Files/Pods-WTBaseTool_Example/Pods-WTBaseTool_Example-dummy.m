515da1e35eab2683083e0daaa61385439edf6cfb
St W <750692607@qq.com>
2018-06-26 15:43:59 +0800

    first commit baseTool code

---
+++ Example/Pods/Target Support Files/Pods-WTBaseTool_Example/Pods-WTBaseTool_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WTBaseTool_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WTBaseTool_Example
+@end
---
+++ Example/Pods/Target Support Files/WTBaseTool/WTBaseTool-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WTBaseTool : NSObject
+@end
+@implementation PodsDummy_WTBaseTool
+@end
